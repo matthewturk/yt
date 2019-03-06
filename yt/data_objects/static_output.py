2e497ba3b9f859a19bba18b6c3bdb8f0ef95f07f
Britton Smith <brittonsmith@gmail.com>
2019-03-05 21:21:56 -0600

    Move ParticleFile class to new file.

--- yt/data_objects/static_output.py
+++ yt/data_objects/static_output.py
@@ -1509,45 +1509,6 @@ def _reconstruct_ds(*args, **kwargs):
     datasets = ParameterFileStore()
     ds = datasets.get_ds_hash(*args)
     return ds
-
-@functools.total_ordering
-class ParticleFile(object):
-    def __init__(self, ds, io, filename, file_id, range = None):
-        self.ds = ds
-        self.io = weakref.proxy(io)
-        self.filename = filename
-        self.file_id = file_id
-        if range is None:
-            range = (None, None)
-        self.start, self.end = range
-        self.total_particles = self.io._count_particles(self)
-        # Now we adjust our start/end, in case there are fewer particles than
-        # we realized
-        if self.start is None:
-            self.start = 0
-        self.end = max(self.total_particles.values()) + self.start
-
-    def select(self, selector):
-        pass
-
-    def count(self, selector):
-        pass
-
-    def _calculate_offsets(self, fields, pcounts):
-        pass
-
-    def __lt__(self, other):
-        if self.filename != other.filename:
-            return self.filename < other.filename
-        return self.start < other.start
-
-    def __eq__(self, other):
-        if self.filename != other.filename:
-            return False
-        return self.start == other.start
-
-    def __hash__(self):
-        return hash((self.filename, self.file_id, self.start, self.end))
         
 
 class ParticleDataset(Dataset):
