fe808cd89194905b49cd6d273de53c902745106f
Matthew Turk <matthewturk@gmail.com>
2021-02-03 08:25:48 -0600

    Using yt notations for phi/theta

--- yt/utilities/lib/spherical_walk.pyx
+++ yt/utilities/lib/spherical_walk.pyx
@@ -51,13 +51,18 @@ cdef int walk_volume_spherical(VolumeContainer *vc,
     cdef double min_bound[3], max_bound[3]
     cdef size_t num_radial_voxels, num_polar_voxels, num_azimuthal_voxels
 
-    for i in range(3):
-        min_bound[i] = vc.left_edge[i]
-        max_bound[i] = vc.right_edge[i]
+    min_bound[0] = vc.left_edge[0]
+    max_bound[0] = vc.right_edge[0]
+
+    min_bound[1] = vc.left_edge[2]
+    max_bound[1] = vc.right_edge[2]
+
+    min_bound[2] = vc.left_edge[1]
+    max_bound[2] = vc.right_edge[1]
 
     num_radial_voxels = vc.dims[0]
-    num_polar_voxels = vc.dims[1]
-    num_azimuthal_voxels = vc.dims[2]
+    num_azimuthal_voxels = vc.dims[1]
+    num_polar_voxels = vc.dims[2]
     cdef double sphere_center[3]
     sphere_center[0] = sphere_center[1] = sphere_center[2] = 0.0
 
@@ -71,8 +76,8 @@ cdef int walk_volume_spherical(VolumeContainer *vc,
 
     for i in range(voxels.size()):
         cur_ind[0] = voxels[i].radial
-        cur_ind[1] = voxels[i].polar
-        cur_ind[2] = voxels[i].azimuthal
+        cur_ind[1] = voxels[i].azimuthal
+        cur_ind[2] = voxels[i].polar
         enter_t = voxels[i].enter_t
         exit_t = voxels[i].exit_t
         sample(vc, ray_origin, ray_direction, enter_t, exit_t, cur_ind, data)
